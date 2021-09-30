10006c95ceca2ae419819596ef0103f5e5c45cd1
Fazreil <fazreil@gmail.com>
2021-10-01 04:25:01 +0800

    initial file

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    //fetch text file
+    $.get('./namelist.txt', function(data) {
+        //split on new lines
+        var lines = data.split('\n');
+
+        list = [];
+        //iterate over lines of file and create a option element
+        for(var i=0;i<lines.length-1;i++) {
+            //create option
+            list.push([""+lines[i],40]);
+            console.log(""+list[i]);
+        }
+
+        list.push(["jack",5]);
+        list.push(["jill",4]);
+
+        WordCloud.minFontSize = "20px"
+        WordCloud(document.getElementById('word_cloud'), { list: list} );
+        console.log("end");
+    });
+});
+
+// WordCloud.minFontSize = "15px"
+// WordCloud(document.getElementById('word_cloud'), { namelist: namelist} );
